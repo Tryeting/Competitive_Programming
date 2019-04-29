135b152eb3a3281f33ff6146e62b77dc3056beb4
Takumi <takumi.o.0410@gmail.com>
2019-04-29 17:53:42 +0900

    掛け算処理追加

---
+++ Competitive_Programming/Modulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Competitive_Programming
+{
+    class Modulo
+    {
+        // 掛け算を行う
+        // Bigmulを使用することで、オーバーフローを抑える
+        // 引数はint32, 戻り値はint64となる
+        public long Mult(int a, int b)
+        {
+            return Math.BigMul(a, b);
+        }
+    }
+}
