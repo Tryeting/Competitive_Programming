ff72032d0f2da28d1c3a2feea97abf4b6ac9e168
Takumi <takumi.o.0410@gmail.com>
2019-04-29 11:40:52 +0900

    プロジェクト ファイルを追加します。

---
+++ Competitive_Programming/Lcm_and_Gcd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Competitive_Programming
+{
+    /*
+    // 最大公約数(Greatest Common Divisor), 最小公倍数(Least Common Multiple)
+    // 二つの整数x, yがあるとき以下の関係が成り立つ
+    //   x * y = gcd(x, y) * lcm(x, y)
+    // ※ gcd(x, y) : xとyの最大公約数, lcm(x, y) : xとyの最小公倍数
+    */
+    class Lcm_and_Gcd
+    {
+        // 最小公倍数を求める
+        public static int Lcm(int x, int y)
+        {
+            return x * y / Gcd(x, y);
+        }
+
+        // ユークリッドの互除法から最大公約数を求める
+        // ユークリッドの互除法の解説：https://www.studyplus.jp/412
+        public static int Gcd(int x, int y)
+        {
+            if (x < y)
+            {
+                return Gcd(y, x);
+            }
+            while (y != 0)
+            {
+                var remainder = y % x;
+                x = y;
+                y = remainder;
+            }
+            return x;
+        }
+    }
+}
---
+++ Competitive_Programming/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Competitive_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            // 標準入力 スペースのない文字列を入力
+            Console.WriteLine("スペースのない数字(int)を入力してください");
+            a = int.Parse(Console.ReadLine());
+            // 2つ以上のスペース区切り入力の取得
+            Console.WriteLine("スペースのある数字(int)を入力してください");
+            string[] str = Console.ReadLine().Split(' ');
+            b = int.Parse(str[0]);
+            c = int.Parse(str[1]);
+            // 改行なし出力
+            Console.Write("改行なし a = {0}, b = {1}, c = {2} \n", a, b, c);
+            // 改行あり出力
+            Console.WriteLine("改行あり a = {0}, b = {1}, c = {2}", a, b, c);
+        }
+    }
+}
